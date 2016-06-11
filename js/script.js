45c71c116885898a5d9d4ade02a79c664a7b7b87
Cedric Ith <cedric.ith1@gmail.com>
2016-06-11 15:24:55 -0700

    updated all files

--- js/script.js
+++ js/script.js
@@ -19,22 +19,29 @@
 	}
 
 	function generateStars () {
-		for (var i = 0; i < 100; i++) {
+		for (var i = 0; i < 200; i++) {
 			// create star
 			var currStar = document.createElement("img");
 			currStar.className = "back stars";
 			currStar.src = "images/star.svg";
 			// set a random size
-			var randomSize = Math.random() * 20;
-			print(randomSize);
-			currStar.width = randomSize + "px";
+			var randomSize = Math.random() * 5;
+			console.log(randomSize);
+			currStar.style.width = randomSize + "px";
 			// set random position
-			var randomTop = Math.random() * $(window).height();
-			currStar.top = randomTop + "px";
-			var randomLeft = Math.random() * $(window).width();
-			currStar.left = randomLeft + "px";
+			var randomTop = Math.random() * 100;
+			currStar.style.top = randomTop + "vh";
+			var randomLeft = Math.random() * 100;
+			currStar.style.left = randomLeft + "vw";
 
 			document.getElementById("SplashSection").appendChild(currStar);
 		}
+
+		// create star
+		var moon = document.createElement("img");
+		moon.className = "back";
+		moon.id = "moon";
+		moon.src = "images/moon.svg";
+		document.getElementById("SplashSection").appendChild(moon);
 	}
 })();
