55ea7221117ce1254b50d01f31685e1170268053
Cedric Ith <cedric.ith1@gmail.com>
2016-06-12 03:31:56 -0700

    Added pink dubhacks logo plus contact links. IT'S SO BEAUTIFULgit pushgit push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 (function(){
 	window.onload = function() {
 		//centerScreen();
-		generateStars();
+		generateBackground();
 	}
 
 	window.onresize = function() {
@@ -18,7 +18,8 @@
 		$("#SplashSection").css("padding-bottom", padding + "px");
 	}
 
-	function generateStars () {
+	function generateBackground () {
+		// creating stars
 		for (var i = 0; i < 200; i++) {
 			// create star
 			var currStar = document.createElement("img");
@@ -26,7 +27,7 @@
 			currStar.src = "images/star.svg";
 			// set a random size
 			var randomSize = Math.random() * 5;
-			console.log(randomSize);
+			//console.log(randomSize);
 			currStar.style.width = randomSize + "px";
 			// set random position
 			var randomTop = Math.random() * 100;
@@ -38,17 +39,14 @@
 		}
 
 		// create moon
-		var moon = document.createElement("img");
-		moon.className = "back";
-		moon.id = "moon";
+		var moon = createNewElement("img", "back", "moon", "");
 		moon.src = "images/moon.svg";
 		document.getElementById("SplashSection").appendChild(moon);
 
 		// create clouds
 		for (var j = 5; j >= 1; j--) {
 			//if (j > 2) {
-				var currCloud = document.createElement("img");
-				currCloud.className = "cloud back";
+				var currCloud = createNewElement("img", "cloud back", "", "");
 				currCloud.src = "images/cloud_" + j + ".svg";
 				document.getElementById("SplashSection").appendChild(currCloud);
 
@@ -61,6 +59,59 @@
 			//}
 			
 		}
+
+		// creating pink dubhacks logo
+		var logo = createNewElement("img", "back", "pink_logo", "");
+		logo.src = "images/dubhacks_logo_pink.svg";
+		document.getElementById("SplashSection").appendChild(logo);
+
 		
+		// creating fb logo
+		var fb = createNewElement("i", "fa fa-facebook-official fa-3x", "fb", "");
+		var fb_link = document.createElement("a");
+		fb_link.href = "https://www.facebook.com/uwhacks/?fref=ts";
+		//twitter_link.target = "_blank";
+		$(fb_link).attr({
+			"aria-hidden": 'true',
+			"target": "_blank"
+		});
+		fb_link.appendChild(fb);
+		document.getElementById("links").appendChild(fb_link);
+
+		// creating twitter logo
+		var twitter = createNewElement("i", "fa fa-twitter fa-3x", "twitter", "");
+		var twitter_link = document.createElement("a");
+		twitter_link.href = "https://twitter.com/DubHacks?lang=en";
+		$(twitter_link).attr({
+			"aria-hidden": 'true',
+			"target": "_blank"
+		});
+		twitter_link.appendChild(twitter);
+		document.getElementById("links").appendChild(twitter_link);
+
+		// creating email logo
+		var envelope = createNewElement("i", "fa fa-envelope fa-3x", "envelope", "");
+		var envelope_link = document.createElement("a");
+		// Need to set email link
+		envelope_link.href = "https://twitter.com/DubHacks?lang=en";
+		$(envelope_link).attr({
+			"aria-hidden": 'true',
+			"target": "_blank"
+		});
+		envelope_link.appendChild(envelope);
+		document.getElementById("links").appendChild(envelope_link);
+		
+
 	}
+
+	// A helper method to create a new element and append it to the DOM faster.
+	function createNewElement (elementType, elementClass, elementID, innerHTML) {
+		var newElement = document.createElement(elementType);
+		newElement.className = elementClass;
+		newElement.id = elementID;
+		newElement.innerHTML = innerHTML;
+		//document.querySelector(appendTag).appendChild(newElement);
+		return newElement;
+	}
+
 })();
