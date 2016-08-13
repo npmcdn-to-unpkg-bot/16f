46db699b404640fc035293f31c37744ababd239d
Mahir Kothary <m.kothary95@gmail.com>
2016-08-13 14:00:04 -0400

    update to use font-awesome

--- js/navbar.js
+++ js/navbar.js
@@ -7,9 +7,7 @@ var $color_band = $("#color_band");
 var $logo = $("#navbar_dubhacks_logo");
 var $header_container = $("#header_container");
 
-var $fb = $("#fb > a > img");
-var $twitter = $("#twitter > a > img");
-var $mail = $("#mail > a > img");
+var $social = $("li i");
 var mobileScrollBack = false;
 var isHoveringOver = false;
 
@@ -20,7 +18,6 @@ $navbar_container.hover(function(){
 
 	$links.css("color", "#2F2F2F");
 	$before_link.css("background-color", "#2F2F2F");
-	// $color_band.css("opacity", "1");
 
 	$logo.attr("src", "/images/final_logo.png");
 
@@ -58,14 +55,12 @@ $(document).scroll(function() {
 
 // Changes color of social media links to pink.
 function colorLogosPink () {
-	$fb.attr("src", "/images/facebook_pink.png");
-	$twitter.attr("src", "/images/twitter_pink.png");
-	$mail.attr("src", "/images/email_pink.png");
+	$social.css("color", "#ea1875");
+	$logo.attr("src", "/images/final_logo.png");
 }
 
 // Changes color of social media links to white.
 function colorLogosWhite () {
-	$fb.attr("src", "/images/facebook_white.png");
-	$twitter.attr("src", "/images/twitter_white.png");
-	$mail.attr("src", "/images/email_white.png");
+	$social.css("color", "white");
+	$logo.attr("src", "/images/dubhacks_logo_white.svg");
 }
